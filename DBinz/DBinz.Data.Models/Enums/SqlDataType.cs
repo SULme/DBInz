735536c5b5b0483d5bc5c230d54cd3b5b3eac747
Moritz Ebenig <MEbenig@sul.de>
2018-06-06 22:56:12 +0200

    SqlDataType enum added

--- DBinz/DBinz.Data.Models/Enums/SqlDataType.cs
+++ DBinz/DBinz.Data.Models/Enums/SqlDataType.cs
@@ -8,8 +8,33 @@ namespace DBinz.Data.Models.Enums
 {
     public enum SqlDataType
     {
+        Bit,
+        TinyInt,
+        SmallInt,
         Int,
         BigInt,
-
+        Real,
+        Float,
+        Decimal,
+        Numeric,
+        Smallmoney,
+        Money,
+        Date,
+        Time,
+        SmallDateTime,
+        DateTime,
+        Datetime2,
+        DateTimeOffset,
+        Char,
+        VarChar,
+        Text,
+        NChar,
+        NVarChar,
+        NText,
+        Binary,
+        VarBinary,
+        Image,
+        TimeStamp,
+        UniqueIdentifier
     }
 }
